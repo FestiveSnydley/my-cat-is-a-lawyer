51d925a2c960fdd9b407d564136643e0f67e829f
mmaldonado18 <mrmaldonado@albany.edu>
2024-04-25 18:00:00 -0400

    Add files via upload

---
+++ Scenes/Evidencescritps/GoMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//MADE BY MARKUS
+public class GoMenu : MonoBehaviour
+{
+    public void GoMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
